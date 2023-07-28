8953d1a733cd18b0180a54e120f67549b6a54bcd
Patricio Parada <paradagpatricio@gmail.com>
2023-07-28 03:18:16 -0400

    I don't want to elaborate, but here it is all the challenge

---
+++ README.md
@@ -0,0 +1,27 @@
+# JavaScript on the Web
+
+## Context
+
+I need to develop a currency calculator, using data from [here.](https://mindicador.cl)
+
+It also needs to show a graph with currency fluctuation from the past 10 days.
+
+## View
+
+Let's check this wonderful piece of software [here.](https://pelafustan.github.io/js_in_web-challenge_06/)
+
+## TODO
+
+* Change color depending on status.
+* Block other options when task is marked as completed or cancelled.
+* I'm quite sure that I'm forgetting somethig, but maybe it isn't important.
+
+## Author
+
+* [Patricio Parada](https://github.com/pelafustan)
+
+## Acknowledgement
+
+* Black coffee
+* Green tea (when tummy hurts)
+* [Desafío Latam](https://desafiolatam.com)
---
+++ assets/js/script.js
@@ -0,0 +1,85 @@
+async function getCurrencyValues(currency) {
+    try {
+        // the API URL
+        const url = `https://mindicador.cl/api/${currency}`;
+
+        // get data from API
+        return await fetch(url)
+            .then(res => res.json())
+            .then(data => data.serie);
+
+    } catch(error) {
+        // in case of not getting value, returning false. This be handled otherwhere
+        return false;
+    }
+}
+
+async function howMany(inputMoney, currencyTarget) {
+    const factor = await getCurrencyValues(currencyTarget);
+
+    if (!factor) {
+        return 'There is no data. Sorry';
+    }
+
+    return inputMoney / factor[0].valor;
+}
+
+async function createGraph(currency) {
+    const currencyData = await getCurrencyValues(currency)
+        .then(data => data.slice(0, 10));
+
+    const dates = currencyData.map(entry => entry.fecha.split('T')[0]);
+    const currencyValue = currencyData.map(entry => entry.valor);
+
+    const chartContainer = document.getElementById('graph-section');
+    const canvas = document.createElement('canvas');
+
+    const width = document.getElementById('converter').clientWidth;
+    canvas.width = width;
+    canvas.height = Math.round(width * 0.5);
+
+    !chartContainer.firstElementChild 
+        ? chartContainer.appendChild(canvas)
+        : chartContainer.replaceChild(canvas, chartContainer.firstElementChild);
+
+    new Chart(canvas, {
+        type: 'line',
+        data: {
+            labels: dates,
+            datasets: [{
+                label: `Last Ten Days ${currency} appreciation`,
+                data: currencyValue,
+            }],
+        },
+    });
+}
+
+document.getElementById('convert-btn').addEventListener('click', async () => {
+    const currencyInput = parseInt(document.getElementById('user-cash').value);
+    const targetCurrency = document.getElementById('currency-selector').value;
+    const showDiv = document.getElementById('show-converted');
+    let symbol;
+    switch (targetCurrency) {
+        case 'euro':
+            symbol = '€';
+            break;
+        case 'uf':
+            symbol = 'UF';
+            break;
+        default:
+            symbol = '$';
+    }
+    showDiv.innerHTML = '';
+
+    await howMany(currencyInput, targetCurrency).then((output) => {
+        typeof output === 'number'
+            ? showDiv.appendChild(
+                  document.createTextNode(
+                      `You have ${symbol}${output.toFixed(2)}`,
+                  ),
+              )
+            : showDiv.appendChild(document.createTextNode(output));
+    });
+
+    await createGraph(targetCurrency);
+});
